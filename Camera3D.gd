091c2fbcc839d295130f4819aa1c8ebc122067d0
Rebecca Neath <62812006+Pandahoro@users.noreply.github.com>
2023-11-21 20:36:48 +0000

    Updating camera
    Adding a smooth camera instead of fixed camera

--- Camera3D.gd
+++ Camera3D.gd
@@ -1,11 +1,30 @@
 extends Camera3D
+# trying to set up the ability to change camera distance and height
+@export_group("Camera distance")
+@export var distance: float = 4.0
+@export var height: float = 2.0
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	#set physics and toplevel to let it move indepently
+	set_physics_process(true)
+	set_as_top_level(true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	#get position of target node and position of camera and vector for camera to stay level
+	var target = get_parent().get_global_transform().origin
+	var pos = get_global_transform().origin
+	var up = Vector3(0,1,0)
+	#calc offset
+	var offset = pos - target
+	#normalise the distance of the camera
+	offset = offset.normalized()*distance
+	offset.y = height
+	#update the current position of the camera
+	pos = target + offset
+	#look at the target node from the cameras position
+	look_at_from_position(pos, target, up)
 	
--- sedanSports2.gd
+++ sedanSports2.gd
@@ -2,9 +2,18 @@ extends VehicleBody3D
 
 var max_rpm = 600
 var max_torque = 100
+var camera
+var character
+
+
 
 func _physics_process(delta):
-	steering = lerp(steering, Input.get_axis("Right", "Left") * 0.4, 5 *  delta)
+	
+	#get the position of the camera
+	camera = get_node("target/Camera3D").get_global_transform()
+	#sets steering using lerp to smooth out the turning
+	steering = lerp(steering, Input.get_axis("Right", "Left") * 0.4, 5 *  delta) 
+	#handling the acceleration of the car, allows for the speed to be tuned
 	var acceleration = Input.get_axis("Forward", "Back")
 	var rpm = abs($BackLeftWheel.get_rpm())
 	$BackLeftWheel.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm)
