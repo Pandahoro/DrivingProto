4a896840029682c2d75b342da22e4b015ab88b52
Rebecca Neath <62812006+Pandahoro@users.noreply.github.com>
2023-11-27 19:10:16 +0000

    Checkpoints
    Added area points for checkpoints, yet to add functionality
    Controller Support
    Changed Lighting
    Added skybox

--- sedanSports2.gd
+++ sedanSports2.gd
@@ -12,7 +12,7 @@ func _physics_process(delta):
 	#get the position of the camera
 	camera = get_node("target/Camera3D").get_global_transform()
 	#sets steering using lerp to smooth out the turning
-	steering = lerp(steering, Input.get_axis("Right", "Left") * 0.4, 5 *  delta) 
+	steering = lerp(steering, Input.get_axis("Right", "Left") * 0.6, 5 *  delta) 
 	#handling the acceleration of the car, allows for the speed to be tuned
 	var acceleration = Input.get_axis("Forward", "Back")
 	var rpm = abs($BackLeftWheel.get_rpm())
